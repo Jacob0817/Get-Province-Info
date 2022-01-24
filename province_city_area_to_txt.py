39c7de8642de590e0d51e9941a7c519d6f5c8ab1
Jacob Zhou <jacob.zzh@outlook.com>
2022-01-24 20:35:45 +0800

    add version detail

--- province_city_area_to_txt.py
+++ province_city_area_to_txt.py
@@ -1,3 +1,13 @@
+# !py
+# -*- encoding: utf-8 -*-
+'''
+@file      : province_city_area_to_txt.py
+@comments  : 
+@time      : 2022/01/24
+@author    : Jacob Zhou <jacob.zzh@outlook.com>
+@ver       : 1.0
+'''
+
 import requests
 from bs4 import BeautifulSoup
 import time
